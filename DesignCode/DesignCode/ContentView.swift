b6db29f70fc0832b46bf5d207e929d1e63a136ed
Dodanto <ohho0728@naver.com>
2020-10-02 15:50:41 +0900

    Gestures and Events

--- DesignCode/DesignCode/ContentView.swift
+++ DesignCode/DesignCode/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
 	@State var show = false
+	@State var viewState = CGSize.zero
 	
     var body: some View {
 		ZStack {
@@ -21,6 +22,7 @@ struct ContentView: View {
 				.cornerRadius(20)
 				.shadow(radius: 20)
 				.offset(x: 0, y: show ? -400 : -40)
+				.offset(x: viewState.width, y: viewState.height)
 				.scaleEffect(0.9)
 				.rotationEffect(.degrees(show ? 0 : 10))
 				.rotation3DEffect(Angle(degrees: 10), axis: (x: 10.0, y: 0, z: 0))
@@ -32,6 +34,7 @@ struct ContentView: View {
 				.cornerRadius(20)
 				.shadow(radius: 20)
 				.offset(x: 0, y: show ? -200 : -20)
+				.offset(x: viewState.width, y: viewState.height)
 				.scaleEffect(0.95)
 				.rotationEffect(Angle.degrees(show ? 0 : 5))
 				.rotation3DEffect(Angle(degrees: 5), axis: (x: 10.0, y: 0, z: 0))
@@ -39,10 +42,22 @@ struct ContentView: View {
 				.animation(.easeInOut(duration: 0.3))
 			
 			CardView()
+				.offset(x: viewState.width, y: viewState.height)
 				.blendMode(.hardLight)
+				.animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0))
 				.onTapGesture {
 					self.show.toggle()
 				}
+				.gesture(
+					DragGesture().onChanged { value in
+						self.viewState = value.translation
+						self.show = true
+					}
+					.onEnded { value in
+						self.viewState = .zero
+						self.show = false
+					}
+				)
 			
 			BottomCardView()
 				.blur(radius: show ? 20 : 0)
