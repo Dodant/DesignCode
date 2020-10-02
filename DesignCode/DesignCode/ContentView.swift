71f6e8ffbd1fcb5d5d7b26cdeaf3835e4029b832
Dodanto <ohho0728@naver.com>
2020-10-02 13:47:07 +0900

    Layout and Stacks

---
+++ DesignCode/DesignCode/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by Junggyun Oh on 2020/10/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+		ZStack {
+			VStack {
+				Spacer()
+			}
+			.frame(width: 300, height: 220)
+			.background(Color.blue)
+			.cornerRadius(20)
+			.shadow(radius: 20)
+			.offset(x: 0, y: -20)
+			
+			VStack {
+			  HStack {
+				VStack(alignment: .leading) {
+				  Text("UI Design!!")
+					.font(.title)
+					.fontWeight(.semibold)
+					  .foregroundColor(.white)
+				  Text("Certificate")
+					  .foregroundColor(Color("accent"))
+				}
+				Spacer()
+				Image("Logo1")
+			  }
+			  .padding(.horizontal, 20)
+			  .padding(.top, 20)
+			  Spacer()
+			  Image("Card1")
+				  .resizable()
+				  .aspectRatio(contentMode: .fill)
+				.frame(width: 300, height: 140, alignment: .top)
+		  }
+		  .frame(width: 340.0, height: 220.0)
+		  .background(Color.black)
+		  .cornerRadius(20)
+		  .shadow(radius: 20)
+			
+			
+		}
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DesignCode/DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Junggyun Oh on 2020/10/02.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
