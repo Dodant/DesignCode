a291fb5349a97d1d2d88a00b151dfc903dc8676c
Dodanto <ohho0728@naver.com>
2020-10-04 16:18:58 +0900

    Dismiss Gesture with Scale

--- DesignCode/DesignCode/CourseList.swift
+++ DesignCode/DesignCode/CourseList.swift
@@ -11,10 +11,11 @@ struct CourseList: View {
 	@State var courses = courseData
 	@State var active = false
 	@State var activeIndex = -1
+	@State var activeView = CGSize.zero
 	
 	var body: some View {
 		ZStack {
-			Color.black.opacity(active ? 0.5 : 0)
+			Color.black.opacity(Double(self.activeView.height / 500))
 				.animation(.linear)
 				.edgesIgnoringSafeArea(.all)
 			
@@ -30,11 +31,12 @@ struct CourseList: View {
 					ForEach(courses.indices, id: \.self) { index in
 						GeometryReader { geometry in
 							CourseView(
-								active: self.$active,
-								show: self.$courses[index].show,
-								activeIndex: self.$activeIndex,
 								course: self.courses[index],
-								index: index
+								index: index,
+								show: self.$courses[index].show,
+								active: self.$active,
+								activeIndex: self.$activeIndex,
+								activeView: self.$activeView
 							)
 							.offset(y: self.courses[index].show ? -geometry.frame(in: .global).minY : 0)
 							.opacity(self.activeIndex != index && self.active ? 0 : 1)
@@ -63,20 +65,21 @@ struct CourseList_Previews: PreviewProvider {
 }
 
 struct CourseView: View {
-	@Binding var active: Bool
-	@Binding var show: Bool
-	@Binding var activeIndex: Int
 	var course: Course
 	var index: Int
+	@Binding var show: Bool
+	@Binding var active: Bool
+	@Binding var activeIndex: Int
+	@Binding var activeView: CGSize
 	
 	var body: some View {
 		ZStack(alignment: .top) {
 			VStack(alignment: .leading, spacing: 30.0) {
-				Text("Now that we have the basic full-screen card animation, we'll animate some new content underneath. Additionally, we'll implement a close button that will dismiss the full view.")
-				Text("About this Course")
+				Text("Take your SwiftUI app to the App Store with advanced techniques like API data, packages and CMS.")
+				Text("About this course")
 					.font(.title).bold()
-				Text("Now that we have the basic full-screen card animation, we'll animate some new content underneath. Additionally, we'll implement a close button that will dismiss the full view.")
-				Text("Now that we have the basic full-screen card animation, we'll animate some new content underneath. Additionally, we'll implement a close button that will dismiss the full view.")
+				Text("This course is unlike any other. We care about design and want to make sure that you get better at it in the process. It was written for designers and developers who are passionate about collaborating and building real apps for iOS and macOS. While it's not one codebase for all apps, you learn once and can apply the techniques and controls to all platforms with incredible quality, consistency and performance. It's beginner-friendly, but it's also packed with design tricks and efficient workflows for building great user interfaces and interactions.")
+				Text("Minimal coding experience required, such as in HTML and CSS. Please note that Xcode 11 and Catalina are essential. Once you get everything installed, it'll get a lot friendlier! I added a bunch of troubleshoots at the end of this page to help you navigate the issues you might encounter.")
 			}
 			.padding(30)
 			.frame(maxWidth: show ? .infinity : screen.width - 60, maxHeight: show ? .infinity : 280, alignment: .top)
@@ -125,6 +128,23 @@ struct CourseView: View {
 			.clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
 			.shadow(color: Color(course.color).opacity(0.3), radius: 20, x: 0, y: 20)
 			//		.animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+			.gesture(
+				show ?
+					DragGesture()
+					.onChanged { value in
+						guard value.translation.height < 300 else { return }
+						guard value.translation.height > 0 else { return }
+						self.activeView = value.translation
+					}
+					.onEnded { value in
+						if self.activeView.height > 50 {
+							self.show = false
+							self.active = false
+							self.activeIndex = -1
+						}
+						self.activeView = .zero
+					} : nil
+			)
 			.onTapGesture {
 				self.show.toggle()
 				self.active.toggle()
@@ -136,7 +156,27 @@ struct CourseView: View {
 			}
 		}
 		.frame(height: show ? screen.height : 280)
+		.scaleEffect(1 - self.activeView.height / 1000)
+		.rotation3DEffect(Angle(degrees: Double(self.activeView.height / 10)), axis: (x: 0.0, y: 10.0, z: 0.0))
+		.hueRotation(Angle(degrees: Double(self.activeView.height)))
 		.animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+		.gesture(
+			show ?
+				DragGesture()
+				.onChanged { value in
+					guard value.translation.height < 300 else { return }
+					guard value.translation.height > 0 else { return }
+					self.activeView = value.translation
+				}
+				.onEnded { value in
+					if self.activeView.height > 50 {
+						self.show = false
+						self.active = false
+						self.activeIndex = -1
+					}
+					self.activeView = .zero
+				} : nil
+		)
 		.edgesIgnoringSafeArea(.all)
 	}
 }
