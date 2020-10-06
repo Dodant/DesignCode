6cd29d303e2a3215ea75da08db314d43c07dc43a
Dodanto <ohho0728@naver.com>
2020-10-06 22:47:45 +0900

    Tap Progress Animation

--- DesignCode/DesignCode/Buttons.swift
+++ DesignCode/DesignCode/Buttons.swift
@@ -5,11 +5,12 @@ struct Buttons: View {
 		VStack(spacing: 50) {
 			RectangleButton()
 			CircleButton()
+			PayButton()
 		}
 		.frame(maxWidth: .infinity, maxHeight: .infinity)
 		.background(Color(#colorLiteral(red: 0.8980392157, green: 0.9333333333, blue: 1, alpha: 1)))
 		.edgesIgnoringSafeArea(.all)
-		.animation(.spring(response: 0.5, dampingFraction: 0.5, blendDuration: 0))
+		.animation(.spring(response: 0.5, dampingFraction: 0.8, blendDuration: 0))
 	}
 }
 
@@ -117,6 +118,70 @@ struct CircleButton: View {
 				}
 			}
 			.onEnded { value in
+				self.press.toggle()
+			}
+		)
+	}
+}
+
+struct PayButton: View {
+	@GestureState var tap = false
+	@State var press = false
+	
+	var body: some View {
+		ZStack {
+			Image("fingerprint")
+				.opacity(press ? 0 : 1)
+				.scaleEffect(press ? 0 : 1)
+			
+			Image("fingerprint-2")
+				.clipShape(Rectangle().offset(y: tap ? 0 : 50))
+				.animation(.easeInOut)
+				.opacity(press ? 0 : 1)
+				.scaleEffect(press ? 0 : 1)
+			
+			Image(systemName: "checkmark.circle.fill")
+				.font(.system(size: 44, weight: .light))
+				.foregroundColor(Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)))
+				.opacity(press ? 1 : 0)
+				.scaleEffect(press ? 1 : 0)
+			
+		}
+		.frame(width: 100, height: 100)
+		.background(
+			ZStack {
+				LinearGradient(gradient: Gradient(colors: [Color(press ? #colorLiteral(red: 0.8980392157, green: 0.9333333333, blue: 1, alpha: 1) : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)), Color(press ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) : #colorLiteral(red: 0.8980392157, green: 0.9333333333, blue: 1, alpha: 1))]), startPoint: .topLeading, endPoint: .bottomTrailing)
+				
+				Circle()
+					.stroke(Color.clear, lineWidth: 10)
+					.shadow(color: Color(press ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) : #colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1)), radius: 3, x: -5, y: -5)
+				
+				Circle()
+					.stroke(Color.clear, lineWidth: 10)
+					.shadow(color: Color(press ? #colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1) : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)), radius: 3, x: 3, y: 3)
+			}
+		)
+		.clipShape(Circle())
+		.overlay(
+			Circle()
+				.trim(from: tap ? 0.001 : 1, to: 1)
+				.stroke(
+					LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)), Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1))]), startPoint: .topLeading, endPoint: .bottomTrailing),
+					style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round))
+				.frame(width: 88, height: 88)
+				.rotationEffect(Angle(degrees: 90))
+				.rotation3DEffect(Angle(degrees: 180), axis: (x: 1, y: 0, z: 0.0))
+				.shadow(color: Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)).opacity(0.3), radius: 5, x: 3, y: 3)
+				.animation(.easeInOut)
+		)
+		.shadow(color: Color(press ? #colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1) : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)), radius: 20, x: -20, y: -20)
+		.shadow(color: Color(press ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) : #colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1)), radius: 20, x: 20, y: 20)
+		.scaleEffect(tap ? 1.2 : 1)
+		.gesture(
+			LongPressGesture().updating($tap) { currentState, gestureState, transaction in
+				gestureState = currentState
+				
+			}.onEnded { value in
 				self.press.toggle()
 			}
 		)
