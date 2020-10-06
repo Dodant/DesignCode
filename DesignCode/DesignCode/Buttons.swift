0d888c00b3bca5cc0819c17f043601e78d1d0067
Dodanto <ohho0728@naver.com>
2020-10-06 22:58:06 +0900

    Haptic Feedback

--- DesignCode/DesignCode/Buttons.swift
+++ DesignCode/DesignCode/Buttons.swift
@@ -1,6 +1,16 @@
 import SwiftUI
 
+func haptic(type: UINotificationFeedbackGenerator.FeedbackType){
+	UINotificationFeedbackGenerator().notificationOccurred(type)
+}
+
+func impact(style: UIImpactFeedbackGenerator.FeedbackStyle){
+	UIImpactFeedbackGenerator(style: style).impactOccurred()
+}
+
 struct Buttons: View {
+	
+	
 	var body: some View {
 		VStack(spacing: 50) {
 			RectangleButton()
@@ -66,12 +76,14 @@ struct RectangleButton: View {
 			.gesture(
 				LongPressGesture(minimumDuration: 0.5, maximumDistance: 10).onChanged { value in
 					self.tap = true
+					impact(style: .heavy)
 					DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
 						self.tap = false
 					}
 				}
 				.onEnded { value in
 					self.press.toggle()
+					haptic(type: .success)
 				}
 			)
 	}
