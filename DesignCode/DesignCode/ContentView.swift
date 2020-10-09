184dd84e0a13895ec36d2ec835c6fec5645fb9ea
Dodant <ohho0506@gmail.com>
2020-10-09 21:51:45 +0900

    Adapting for iPad

--- DesignCode/DesignCode/ContentView.swift
+++ DesignCode/DesignCode/ContentView.swift
@@ -29,7 +29,8 @@ struct ContentView: View {
 				)
 			
 			BackCardView()
-				.frame(width: showCard ? 300 : 340, height: 220)
+				.frame(maxWidth: showCard ? 300 : 340)
+				.frame(height: 220)
 				.background(show ? Color("card3") : Color("card4"))
 				.cornerRadius(20)
 				.shadow(radius: 20)
@@ -39,12 +40,13 @@ struct ContentView: View {
 				.scaleEffect(showCard ? 1 : 0.9)
 				.rotationEffect(.degrees(show ? 0 : 10))
 				.rotationEffect(Angle(degrees: showCard ? -10 : 0))
-				.rotation3DEffect(Angle(degrees: showCard ? 0 : 10), axis: (x: 10.0, y: 0, z: 0))
+//				.rotation3DEffect(Angle(degrees: showCard ? 0 : 10), axis: (x: 10.0, y: 0, z: 0))
 				.blendMode(.hardLight)
 				.animation(.easeInOut(duration: 0.3))
 			
 			BackCardView()
-				.frame(width: 340, height: 220)
+				.frame(maxWidth: 340)
+				.frame(height: 220)
 				.background(show ? Color("card4") : Color("card3"))
 				.cornerRadius(20)
 				.shadow(radius: 20)
@@ -54,12 +56,13 @@ struct ContentView: View {
 				.scaleEffect(showCard ? 1 : 0.95)
 				.rotationEffect(Angle.degrees(show ? 0 : 5))
 				.rotationEffect(Angle(degrees: showCard ? -5 : 0))
-				.rotation3DEffect(Angle(degrees: showCard ? 0 : 5), axis: (x: 10.0, y: 0, z: 0))
+//				.rotation3DEffect(Angle(degrees: showCard ? 0 : 5), axis: (x: 10.0, y: 0, z: 0))
 				.blendMode(.hardLight)
 				.animation(.easeInOut(duration: 0.3))
 			
 			CardView()
-				.frame(width: showCard ? 375 : 340.0, height: 220.0)
+				.frame(maxWidth: showCard ? 375 : 340.0)
+				.frame(height: 220.0)
 				.background(Color.black)
 //				.cornerRadius(20)
 				.clipShape(RoundedRectangle(cornerRadius: showCard ? 30 : 20, style: .continuous))
@@ -84,34 +87,41 @@ struct ContentView: View {
 			
 //			Text("Value \(bottomState.height)").offset(y: -300)
 			
-			BottomCardView(show: $showCard)
-				.offset(x: 0, y: showCard ? 360 : 1000)
-				.offset(y: bottomState.height)
-				.blur(radius: show ? 20 : 0)
-				.animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
-				.gesture(
-					DragGesture().onChanged { value in
-						self.bottomState = value.translation
-						if showFull {
-							self.bottomState.height += -300
-						}
-						if self.bottomState.height < -300 {
-							self.bottomState.height = -300
-						}
-					}
-					.onEnded { value in
-						if self.bottomState.height > 50 {
-							self.showCard = false
-						}
-						if (self.bottomState.height < -100 && !self.showFull) || (self.bottomState.height < -250 && self.showFull) {
-							self.bottomState.height = -300
-							self.showFull = true
-						} else {
-							self.bottomState = .zero
-							self.showFull = false
-						}
-					}
+			GeometryReader { bounds in
+				BottomCardView(show: $showCard)
+					.offset(x: 0, y: showCard ?
+								bounds.size.height / 2 :
+								bounds.size.height +
+								bounds.safeAreaInsets.top +
+								bounds.safeAreaInsets.bottom)
+				  .offset(y: bottomState.height)
+				  .blur(radius: show ? 20 : 0)
+				  .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
+				  .gesture(
+					  DragGesture().onChanged { value in
+						  self.bottomState = value.translation
+						  if showFull {
+							  self.bottomState.height += -300
+						  }
+						  if self.bottomState.height < -300 {
+							  self.bottomState.height = -300
+						  }
+					  }
+					  .onEnded { value in
+						  if self.bottomState.height > 50 {
+							  self.showCard = false
+						  }
+						  if (self.bottomState.height < -100 && !self.showFull) || (self.bottomState.height < -250 && self.showFull) {
+							  self.bottomState.height = -300
+							  self.showFull = true
+						  } else {
+							  self.bottomState = .zero
+							  self.showFull = false
+						  }
+					  }
 				)
+			}
+//			.edgesIgnoringSafeArea(.all)
 		}
     }
 }
@@ -119,6 +129,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+			.previewLayout(.fixed(width: 320, height: 667))
     }
 }
 
@@ -167,6 +178,9 @@ struct TitleView: View {
 			}
 			.padding()
 			Image("Background1")
+				.resizable()
+				.aspectRatio(contentMode: .fit)
+				.frame(maxWidth: 375)
 			Spacer()
 		}
 	}
@@ -207,10 +221,11 @@ struct BottomCardView: View {
 		}
 		.padding(.top, 8)
 		.padding(.horizontal, 20)
-		.frame(maxWidth: .infinity)
+		.frame(maxWidth: 712)
 		.background(BlurView(style: .systemThinMaterial))
 		.cornerRadius(30)
 		.shadow(radius: 20)
+		.frame(maxWidth: .infinity)
 		
 	}
 }
--- DesignCode/DesignCode/HomeView.swift
+++ DesignCode/DesignCode/HomeView.swift
@@ -13,74 +13,76 @@ struct HomeView: View {
 	@Binding var showContent: Bool
 	
 	var body: some View {
-		ScrollView {
-			VStack {
-				HStack {
-					Text("Watching")
-						//.font(.system(size: 40, weight: .bold))
-						.modifier(CustomFontModifier(size: 35))
-					
-					Spacer()
-					
-					AvatarView(showProfile: $showProfile)
-						.shadow(color: Color.black.opacity(0.2), radius: 10, x: 1, y: 10)
-					
-					Button(action: { self.showUpdate.toggle() }) {
-						Image(systemName: "bell")
-//							.renderingMode(.original)
-							.foregroundColor(.primary)
-							.font(.system(size: 16, weight: .medium))
-							.frame(width: 36, height: 36)
-							.background(Color("background3"))
-							.clipShape(Circle())
-							.modifier(ShadowModifier())
-						
-					}
-					.sheet(isPresented: $showUpdate, content: {
-						UpdateList()
-					})
-				}
-				.padding(.horizontal)
-				.padding(.top, 30)
-				
-				ScrollView(.horizontal, showsIndicators: false) {
-					WatchRingView()
-						.padding(.horizontal, 30)
-						.padding(.bottom, 30)
-						.onTapGesture {
-							self.showContent = true
-						}
-				}
-				
-				ScrollView(.horizontal, showsIndicators: false) {
-					HStack(spacing: 20) {
-						ForEach(sectionData) { item in
-							GeometryReader { geometry in
-								SectionView(section: item)
-									.rotation3DEffect(Angle(degrees: Double(geometry.frame(in: .global).minX - 30) / -20), axis: (x: 0, y: 10, z: 0))
-							}
-							.frame(width: 275, height: 275)
-						}
-					}
-					.padding(30)
-					.padding(.leading, 14)
-					.padding(.bottom, 30)
-				}
-				.offset(y: -30)
-				
-				HStack {
-					Text("Courses")
-						.font(.title).bold()
-					Spacer()
-				}
-				.padding(.leading, 30)
-				.offset(y: -60)
-				
-				SectionView(section: sectionData[2], width: screen.width - 60, height: 275)
-					.offset(y: -50)
-				Spacer()
+		GeometryReader { bounds in
+			ScrollView {
+			  VStack {
+				  HStack {
+					  Text("Watching")
+						  //.font(.system(size: 40, weight: .bold))
+						  .modifier(CustomFontModifier(size: 35))
+					  
+					  Spacer()
+					  
+					  AvatarView(showProfile: $showProfile)
+						  .shadow(color: Color.black.opacity(0.2), radius: 10, x: 1, y: 10)
+					  
+					  Button(action: { self.showUpdate.toggle() }) {
+						  Image(systemName: "bell")
+  //							.renderingMode(.original)
+							  .foregroundColor(.primary)
+							  .font(.system(size: 16, weight: .medium))
+							  .frame(width: 36, height: 36)
+							  .background(Color("background3"))
+							  .clipShape(Circle())
+							  .modifier(ShadowModifier())
+						  
+					  }
+					  .sheet(isPresented: $showUpdate, content: {
+						  UpdateList()
+					  })
+				  }
+				  .padding(.horizontal)
+				  .padding(.top, 30)
+				  
+				  ScrollView(.horizontal, showsIndicators: false) {
+					  WatchRingView()
+						  .padding(.horizontal, 30)
+						  .padding(.bottom, 30)
+						  .onTapGesture {
+							  self.showContent = true
+						  }
+				  }
+				  
+				  ScrollView(.horizontal, showsIndicators: false) {
+					  HStack(spacing: 20) {
+						  ForEach(sectionData) { item in
+							  GeometryReader { geometry in
+								  SectionView(section: item)
+									  .rotation3DEffect(Angle(degrees: Double(geometry.frame(in: .global).minX - 30) / -20), axis: (x: 0, y: 10, z: 0))
+							  }
+							  .frame(width: 275, height: 275)
+						  }
+					  }
+					  .padding(30)
+					  .padding(.leading, 14)
+					  .padding(.bottom, 30)
+				  }
+				  .offset(y: -30)
+				  
+				  HStack {
+					  Text("Courses")
+						  .font(.title).bold()
+					  Spacer()
+				  }
+				  .padding(.leading, 30)
+				  .offset(y: -60)
+				  
+				SectionView(section: sectionData[2], width: bounds.size.width - 60, height: 275)
+					  .offset(y: -50)
+				  Spacer()
+			  }
+			.frame(width: bounds.size.width)
 			}
-			.frame(width: screen.width)
 		}
 	}
 }
@@ -88,6 +90,7 @@ struct HomeView: View {
 struct HomeView_Previews: PreviewProvider {
 	static var previews: some View {
 		HomeView(showProfile: .constant(false), showContent: .constant(false))
+			.environmentObject(UserStore())
 	}
 }
 
