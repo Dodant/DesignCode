c5060d26e919a2e1638d09680db81cbdd56e165d
Dodant <ohho0506@gmail.com>
2020-10-09 13:08:10 +0900

    Firebase link

--- DesignCode/DesignCode/DesignCodeApp.swift
+++ DesignCode/DesignCode/DesignCodeApp.swift
@@ -6,12 +6,23 @@
 //
 
 import SwiftUI
+import UIKit
+import Firebase
 
 @main
 struct DesignCodeApp: App {
+	@UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+	
     var body: some Scene {
         WindowGroup {
             LoginView()
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+		FirebaseApp.configure()
+		return true
+	}
 }
