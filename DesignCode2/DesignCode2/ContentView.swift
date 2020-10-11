fda87149d67d5a323b097d0b29b76eadbeb3f06e
Dodant <ohho0506@gmail.com>
2020-10-11 15:42:31 +0900

    SwiftUI Visual Editor

--- DesignCode2/DesignCode2/ContentView.swift
+++ DesignCode2/DesignCode2/ContentView.swift
@@ -1,21 +1,38 @@
-//
-//  ContentView.swift
-//  DesignCode2
-//
-//  Created by Junggyun Oh on 2020/10/11.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+	var body: some View {
+		VStack(alignment: .leading, spacing: 4.0) {
+			Spacer()
+			HStack {
+				Spacer()
+				Image("Illustration 1")
+				  .resizable()
+					.aspectRatio(contentMode: .fit)
+				Spacer()
+			}
+			Text("SwiftUI for iOS14")
+				.fontWeight(.bold)
+				.foregroundColor(Color.white)
+			Text("20 Sections")
+				.font(.footnote)
+				.foregroundColor(Color.white)
+		}
+		.padding(.all)
+		.background(Color.blue)
+		.cornerRadius(20.0)
+		.shadow(radius: 10)
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		Group {
+			ContentView()
+				
+			ContentView()
+				.previewLayout(.fixed(width: 200.0, height: 200.0))
+				.preferredColorScheme(.dark)
+		}
+	}
 }
