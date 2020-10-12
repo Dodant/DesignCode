c6b4a5432c0c0beb4c647f5e843c8e533962d55e
Dodanto <ohho0506@gmail.com>
2020-10-12 17:23:38 +0900

    Build for macOS Big Sur

--- DesignCode2/Shared/CoursesView.swift
+++ DesignCode2/Shared/CoursesView.swift
@@ -8,13 +8,23 @@
 import SwiftUI
 
 struct CoursesView: View {
+	@ViewBuilder
     var body: some View {
+		#if os(iOS)
+		content
+			.listStyle(InsetGroupedListStyle())
+		#else
+		content
+			.frame(minWidth: 800, minHeight: 600)
+		#endif
+    }
+	
+	var content: some View{
 		List(0 ..< 20) { item in
 			CourseRow()
 		}
-		.listStyle(InsetGroupedListStyle())
 		.navigationTitle("Courses")
-    }
+	}
 }
 
 struct CoursesView_Previews: PreviewProvider {
--- DesignCode2/Shared/Sidebar.swift
+++ DesignCode2/Shared/Sidebar.swift
@@ -3,20 +3,30 @@ import SwiftUI
 struct Sidebar: View {
 	var body: some View {
 		NavigationView {
-			List {
-				NavigationLink(destination: CoursesView()){
-					Label("Course", systemImage: "book.closed")
-				}
-				Label("Tutorial", systemImage: "list.bullet.rectangle")
-				Label("Livestream", systemImage: "tv")
-				Label("Certificates", systemImage: "mail.stack")
-				Label("Search", systemImage: "magnifyingglass")
-			}
-			.listStyle(SidebarListStyle())
-			.navigationTitle("Learn")
+			#if os(iOS)
+			content
+				.navigationTitle("Learn")
+			#else
+			content
+				.frame(minWidth: 200, idealWidth: 250, maxWidth: 300)
+			#endif
 			
 			CoursesView()
+			CourseRow()
 		}
+	}
+	
+	var content: some View {
+		List {
+			NavigationLink(destination: CoursesView()){
+				Label("Course", systemImage: "book.closed")
+			}
+			Label("Tutorial", systemImage: "list.bullet.rectangle")
+			Label("Livestream", systemImage: "tv")
+			Label("Certificates", systemImage: "mail.stack")
+			Label("Search", systemImage: "magnifyingglass")
+		}
+		.listStyle(SidebarListStyle())
 		
 	}
 }
@@ -24,9 +34,7 @@ struct Sidebar: View {
 struct Sidebar_Previews: PreviewProvider {
 	static var previews: some View {
 		Group {
-//			Sidebar()
 			Sidebar()
-				.previewDevice("iPad Pro (11-inch) (2nd generation)")
 		}
 	}
 }
