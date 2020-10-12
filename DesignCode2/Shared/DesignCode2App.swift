ba0c8895ef289496cca01fb657a588c71a8bc2dc
Dodanto <ohho0506@gmail.com>
2020-10-12 15:08:23 +0900

    Build a SwiftUI app for iOS14

---
+++ DesignCode2/Shared/DesignCode2App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCode2App.swift
+//  Shared
+//
+//  Created by Junggyun Oh on 2020/10/12.
+//
+
+import SwiftUI
+
+@main
+struct DesignCode2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
