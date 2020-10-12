27315434f6d282fcf30bf7b2d3de34e43b49c292
Dodanto <ohho0506@gmail.com>
2020-10-12 17:57:03 +0900

    Matched Geometry Effect

--- DesignCode2/Shared/CoursesView.swift
+++ DesignCode2/Shared/CoursesView.swift
@@ -1,13 +1,32 @@
 import SwiftUI
 
 struct CoursesView: View {
+	@State var show = false
 	var body: some View {
-		Text("")
+		ZStack {
+			CourseItem()
+				.frame(width: 335, height: 250)
+			VStack {
+				if show {
+					CourseItem()
+						.transition(.move(edge: .leading))
+						.edgesIgnoringSafeArea(.all)
+					
+				}
+			}
+			
+		}
+		.onTapGesture {
+			withAnimation(.spring()) {
+				show.toggle()
+			}
+		}
+//		.animation(.spring())
 	}
 }
 
 struct CoursesView_Previews: PreviewProvider {
-    static var previews: some View {
-        CoursesView()
-    }
+	static var previews: some View {
+		CoursesView()
+	}
 }
