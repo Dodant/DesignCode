a6fb4dee08c2a9a208068b498bb0724e4cb410e7
Dodanto <ohho0506@gmail.com>
2020-10-17 16:36:00 +0900

    Lazy Grid Layout

--- DesignCode2/Shared/CoursesView.swift
+++ DesignCode2/Shared/CoursesView.swift
@@ -9,11 +9,14 @@ struct CoursesView: View {
 	var body: some View {
 		ZStack {
 			ScrollView {
-				VStack(spacing: 20.0) {
+				LazyVGrid(
+					columns: [GridItem(.adaptive(minimum: 160), spacing: 16)],
+					spacing: 16
+				) {
 					ForEach(courses) { item in
 						CourseItem(course: item)
 							.matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
-							.frame(width: 335, height: 250)
+							.frame(height: 200)
 							.onTapGesture {
 								withAnimation(.spring()) {
 									show.toggle()
@@ -22,10 +25,9 @@ struct CoursesView: View {
 								}
 							}
 							.disabled(isDisabled)
-						
 					}
-					
 				}
+				.padding(16)
 				.frame(maxWidth: .infinity)
 			}
 			
